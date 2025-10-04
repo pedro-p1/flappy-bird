ead1dd7479236eedd15af4cb795820caa51d3d60
pedro-p1 <pedro.elzein@gmail.com>
2025-10-04 23:43:27 +0100

    added collision for bird and pipe

---
+++ scenes/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+		
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		self.hide()	#This is what happens when bird collides
+
+	move_and_slide()
