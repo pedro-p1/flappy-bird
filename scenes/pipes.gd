43abd0fb6be73c81d9e2a135827f6c72060c7c14
pedro-p1 <pedro.elzein@gmail.com>
2025-10-07 21:12:38 +0100

    poles creating perfectly

--- scenes/pipes.gd
+++ scenes/pipes.gd
@@ -6,23 +6,20 @@ var xcoord: int = 0
 var pipe = preload("res://scenes/pipe.tscn")
 @onready var pipes: Node2D = $"."
 
-func createPipe(delta):
-	for i in range(50):
+func createPipe():
+	for i in range(1000000000):
 		var newPipe = pipe.instantiate()
-		height = randi_range(500, 1000)
+		height = randi_range(500, 940)
 		xcoord = 3000 + i * 800
 		newPipe.position = Vector2(xcoord, height)
 		add_child(newPipe)
-		
-		while pipes.position.x != -MOVESPEED * delta + i * 800:
-			pass
+		await get_tree().create_timer(4).timeout
 
 func _ready() -> void:
-	pass
+	createPipe()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	self.move_local_x(-MOVESPEED * delta)
 	
-	
-	createPipe(delta)
