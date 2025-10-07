9a52b78b490528d8f2d8c44a06a0e6bbde2bbc9c
pedro-p1 <pedro.elzein@gmail.com>
2025-10-07 20:42:14 +0100

    added pole instanciate, working on timing

--- scenes/pipes.gd
+++ scenes/pipes.gd
@@ -3,20 +3,26 @@ extends Node2D
 const MOVESPEED = 200 # speed that the pipes move
 var height: int = 0 # range of the y coord
 var xcoord: int = 0
-@export var pipe_scene: PackedScene
+var pipe = preload("res://scenes/pipe.tscn")
+@onready var pipes: Node2D = $"."
+
+func createPipe(delta):
+	for i in range(50):
+		var newPipe = pipe.instantiate()
+		height = randi_range(500, 1000)
+		xcoord = 3000 + i * 800
+		newPipe.position = Vector2(xcoord, height)
+		add_child(newPipe)
+		
+		while pipes.position.x != -MOVESPEED * delta + i * 800:
+			pass
 
 func _ready() -> void:
-	for i in range(10):
-		var newPipe = pipe_scene.instantiate()
-		height = randi_range(500, 1000)
-		xcoord = 1500 + i * 500
-		newPipe.position = Vector2(xcoord, height)
-		print("created pipe number:", i)
-		print("coords are: ", str(xcoord) + "," + str(height))
-		await get_tree().create_timer(5).timeout
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	self.move_local_x(-MOVESPEED * delta)
 	
+	
+	createPipe(delta)
--- scenes/worldboundaries.gd
+++ scenes/worldboundaries.gd
@@ -4,7 +4,3 @@ extends StaticBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("world boundaries")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
